cf94936a336a9977f600152343a4b7ef69e5b87e
khoaitaychiengion <86716473+khoaitaychiengion@users.noreply.github.com>
2023-11-10 23:52:59 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 const CONFIG = {
     introTitle: 'Hé nhô Lùn !!!',
     introDesc: 'Lại là PTB đây 😀',
-    btnIntro: 'Nì tao chuẩn bị cho mày đó 😙',
+    btnIntro: 'Cái này là mụi chuẩn bị cho quynh đó 😙',
     title: 'Xao xao, quynh có bất ngờ khom chứ hả ?? 😎',
-    desc: 'Mày chọn "Không" được tao cùi á Lùn 😉',
+    desc: 'Mày chọn "Không bất ngờ" được tao cùi luôn á Lùn 😉',
     btnYes: 'Không ngờ luôn á, PTB mãi đỉnkkk 😘',
     btnNo: 'Thấy cũng bình thường =)))',
-    question:'Mày thấy sao nếu tao nói tao hack được cái bàn phím của mày 🫠',
+    question:'Mày nghĩ sao nếu tao nói là tao hack được cái bàn phím của mày 🫠',
     btnReply: 'Điều vui còn ở phía trước',
-    reply: 'Tao đã nói là hack được mà. Đừng có cố nữa, chấp nhận đii 😇',
+    reply: 'Tao đã nói là hack được mà. Đừng có cố nữa, chấp nhận sự thật đii 😇',
     mess: 'HAPPY BIRTHDAY TO LÙN 🎂🎂',
-    messDesc: 'Cả ngày không nói tiếng nào là có lí dó nghen !!!',
+    messDesc: 'Cả ngày không nói tiếng nào là có lí do nghe !!',
     btnAccept: 'Chưa hết, chưa hết 😙😙',
     messLink: 'https://sites.google.com/view/sinh-nhat-lun'
 }

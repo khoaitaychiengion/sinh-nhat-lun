99290da2495000205f13f03c3e672f9f78b6f0cf
khoaitaychiengion <86716473+khoaitaychiengion@users.noreply.github.com>
2023-11-16 20:42:06 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -104,7 +104,7 @@ $('#yes').click(function() {
         html: true,
         width: 900,
         padding: '3em',
-        html: "<input type='text' class='form-control' id='txtReason' onmousemove=textGenerate()  placeholder='Chắc mày nghĩ tao bị khùng'>",
+        html: "<input type='text' class='form-control' id='txtReason' onmousemove=textGenerate()  placeholder='Chắc mày nghĩ tao khùng'>",
         background: '#fff url("img/iput-bg.jpg")',
         backdrop: `
               rgba(0,0,123,0.4)
